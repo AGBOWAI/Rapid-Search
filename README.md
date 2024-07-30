0119d62d48004abb7c6e9771571557996de14084
Juste AGBOWAI <agbowaijuste01@gmail.com>
2024-07-30 11:49:21 +0200

    Last

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 
 Rapid Search est une extension de navigateur compatible avec n'importe quel navigateur. Il vous permet de rechercher très rapidement une information en tapant seulement le nom de celui-ci dans la barre de recherche et de choisir le moteur de recherche que vous vouliez.
-Il est très simple et efficace et vous permet également de ne pas toujours aller sur la page de Google ou tout autre pour rechercher vos infoemations.
+Il est très simple et efficace et vous permet également de ne pas toujours aller sur la page de Google ou tout autre pour rechercher vos informations.
     Rapid Seach n'est qu'a sa première version...so, Stay tunned
--- extens.js
+++ extens.js
@@ -10,7 +10,15 @@ document.getElementById("google").addEventListener("click", () => {
 document.getElementById("wikipedia").addEventListener("click", () => {
     const query = document.getElementById("searchBox").value
     if(query){
-        const urlwiki = `https://en.wikipedia.org/wiki/Special:Search?search=${ encodeURIComponent(query)}`
+        const urlwiki = `https://fr.wikipedia.org/wiki/Special:Search?search=${ encodeURIComponent(query)}`
         chrome.tabs.create({url: urlwiki})
+    }
+})
+
+document.getElementById("youtube").addEventListener("click", () => {
+    const query = document.getElementById("searchBox").value
+    if(query){
+        const urlyout = `https://www.youtube.com/results?search_query=${encodeURIComponent(query)}`
+        chrome.tabs.create({url: urlyout})
     }
 })
